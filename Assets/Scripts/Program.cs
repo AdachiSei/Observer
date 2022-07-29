76d987a98969f83121a7421320f203a4be0f4048
DESKTOP-P0CK6VC\Sei Adachi <sei.dance.dream@gmail.com>
2022-07-29 14:53:14 +0900

    テストコード修正

--- Assets/Scripts/Program.cs
+++ Assets/Scripts/Program.cs
@@ -5,7 +5,14 @@ using UnityEngine;
 
 public class Program : MonoBehaviour
 {
-    static void Main(string[] args)
+    string[] _test = {"a","b"};
+
+    private void Start()
+    {
+        MainTest(_test);
+    }
+
+    static public void MainTest(string[] args)
     {
         //値を受け取るクラスを３つ作成
         Observer observerA = new Observer("Aさん");
@@ -19,7 +26,7 @@ public class Program : MonoBehaviour
         IDisposable disposableA = observable.Subscribe(observerA);
         IDisposable disposableB = observable.Subscribe(observerB);
         IDisposable disposableC = observable.Subscribe(observerC);
-        Console.WriteLine("Aさん〜Cさんが値を購読しました");
+        Debug.Log("Aさん〜Cさんが値を購読しました");
 
         Debug.Log("値を発行させます");
         //Observableに値を発行させる
